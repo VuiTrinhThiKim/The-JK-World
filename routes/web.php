a380f36af1f44e867bea294b13c0fa98d2d19d37
VuiTrinhThiKim <1851010157vui@ou.edu.vn>
2021-08-21 12:30:13 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,71 +29,73 @@ Route::get('/chi-tiet-san-pham/{product_id}', 'ProductController@show_product_de
 Route::prefix('admin')->group(function(){
 
 	//Show Login Admin Panel
-	Route::get('/', 'AdminController@index');
+	Route::get('/', [AdminController::class, 'index']);
 	//Show Admin Panel
-	Route::get('/dashboard', 'AdminController@showDashboard');
+	Route::get('/dashboard', [AdminController::class, 'showDashboard']);
 	//Login to dashboard
-	Route::post('/admin-dashboard', 'AdminController@dashboard');
+	Route::post('/admin-dashboard', [AdminController::class, 'dashboard']);
 	//Admin Logout
-	Route::get('/logout', 'AdminController@logout');
+	Route::get('/logout', [AdminController::class, 'logout']);
 
 	Route::group(['prefix'=>'category'], function(){
 		//All Categories
-		Route::get('/view-all', 'CategoryController@view_all');
-		//Add Category
-		Route::get('/add', 'CategoryController@add');
+		Route::get('/view-all', [CategoryController::class, 'view_all']);
+		//Create Category
+		Route::get('/add', [CategoryController::class, 'create']);
+		//Add to db
+		Route::post('/add', [CategoryController::class, 'store']);
 
 		//Category Status
-		Route::get('/public-category/{category_id}', 'CategoryController@update_category_status');
-		Route::get('/unpublic-category/{category_id}', 'CategoryController@update_category_status');
+		Route::get('/public-category/{category_id}', [CategoryController::class, 'update_category_status']);
+		Route::get('/unpublic-category/{category_id}', [CategoryController::class, 'update_category_status']);
 		//Edit Category
-		Route::get('/edit/{category_id}', 'CategoryController@edit');
+		Route::get('/edit/{category_id}', [CategoryController::class, 'edit']);
 		//Update Category
-		Route::post('/update/{category_id}', 'CategoryController@update');
+		Route::post('/update/{category_id}', [CategoryController::class, 'update']);
 		//Delete Category
-		Route::get('/delete/{category_id}', 'CategoryController@delete');
+		Route::get('/delete/{category_id}', [CategoryController::class, 'destroy']);
 		//Save Category
-		Route::post('/save', 'CategoryController@save');
 
 	});
 
 	Route::group(['prefix'=>'brand'], function(){
 		//All Brands
-		Route::get('/view-all', 'BrandController@view_all');
-		//Add Brand
-		Route::get('/add', 'BrandController@add');
+		Route::get('/view-all', [BrandController::class, 'view_all']);
+		//Create Brand
+		Route::get('/add', [BrandController::class, 'create']);
+		//Add to db
+		Route::post('/add', [BrandController::class, 'store']);
 
 		//Brand Status
-		Route::get('/public-brand/{brand_id}', 'BrandController@update_brand_status');
-		Route::get('/unpublic-brand/{brand_id}', 'BrandController@update_brand_status');
+		Route::get('/public-brand/{brand_id}', [BrandController::class, 'update_brand_status']);
+		Route::get('/unpublic-brand/{brand_id}', [BrandController::class, 'update_brand_status']);
 		//Edit Brand
-		Route::get('/edit/{brand_id}', 'BrandController@edit');
+		Route::get('/edit/{brand_id}', [BrandController::class, 'edit']);
 		//Update Brand
-		Route::post('/update/{brand_id}', 'BrandController@update');
+		Route::post('/update/{brand_id}', [BrandController::class, 'update']);
 		//Delete Brand
-		Route::get('/delete/{brand_id}', 'BrandController@delete');
-		//Save Brand
-		Route::post('/save', 'BrandController@save');
+		Route::get('/delete/{brand_id}', [BrandController::class, 'destroy']);
 
 	});
 
 	Route::group(['prefix'=>'product'], function(){
-		//All Brands
-		Route::get('/view-all', 'ProductController@view_all');
-		//Add Brand
-		Route::get('/add', 'ProductController@add');
+		//All Product
+		Route::get('/view-all', [ProductController::class, 'view_all']);
+		//Add Product
+		Route::get('/add', [ProductController::class, 'create']);
+		//Add to db
+		Route::post('/add', [ProductController::class, 'store']);
 
-		//Brand Status
-		Route::get('/public-product/{product_id}', 'ProductController@update_product_status');
-		Route::get('/unpublic-product/{product_id}', 'ProductController@update_product_status');
-		//Edit Brand
-		Route::get('/edit/{product_id}', 'ProductController@edit');
-		//Update Brand
-		Route::post('/update/{product_id}', 'ProductController@update');
-		//Delete Brand
-		Route::get('/delete/{product_id}', 'ProductController@delete');
-		//Save Brand
-		Route::post('/save', 'ProductController@save');
+
+		//Product Status
+		Route::get('/public-product/{product_id}', [ProductController::class, 'update_product_status']);
+		Route::get('/unpublic-product/{product_id}', [ProductController::class, 'update_product_status']);
+		//Edit Product
+		Route::get('/edit/{product_id}', [ProductController::class, 'edit']);
+		//Update Product
+		Route::post('/update/{product_id}', [ProductController::class, 'update']);
+		//Delete Product
+		Route::get('/delete/{product_id}', [ProductController::class, 'destroy']);
 
 	});
 });
