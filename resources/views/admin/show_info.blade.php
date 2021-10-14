97eaff51ae2811c82b23515e071bd225f710b20a
Vui Trinh Thi Kim <1851010157vui@ou.edu.vn>
2021-10-14 22:06:00 +0700

    Update layout

--- resources/views/admin/show_info.blade.php
+++ resources/views/admin/show_info.blade.php
@@ -63,7 +63,7 @@
                                 <input type="text" class="form-control" id="adPhone" name="adPhone" value="{{$admin->phone}}">
                             </div>
                             <div class="form-group">
-                                <label for="adAddress">Địa chỉ</label>
+                                <label for="adPhone">Địa chỉ</label>
                                 <textarea style="resize: none;" rows=4 class="form-control" id="adAddress" name="adAddress"placeholder="Nhập thông tin" >{{$admin->address}}</textarea>
                             </div>
                             <div class="form-group update-avt">
